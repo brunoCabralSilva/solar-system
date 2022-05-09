673e5ae47da39a32fa31bea079fe2008bf585957
brunoCabralSilva <bruno.cabral.silva2018@gmail.com>
2022-05-09 17:16:03 -0300

    Requisitos 5 e 6

--- src/components/PlanetCard.js
+++ src/components/PlanetCard.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import '../App.css';
+import PropTypes from 'prop-types';
 
 class PlanetCard extends React.Component {
   render() {
+    const { planetName } = this.props;
+    const { planetImage } = this.props;
+    const texto = `Planeta ${planetName}`;
     return (
-      <header>
-        <div data-testid="planet-card">
-          Mopa
-        </div>
-      </header>
+      <div data-testid="planet-card">
+        <p data-testid="planet-name">{planetName}</p>
+        <img src={ planetImage } alt={ texto } />
+      </div>
     );
   }
 }
+
+PlanetCard.propTypes = {
+  planetName: PropTypes.string.isRequired,
+  planetImage: PropTypes.string.isRequired,
+};
 
 export default PlanetCard;
--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import '../App.css';
 import Title from './Title';
+import Planets from '../data/planets';
+import PlanetCard from './PlanetCard';
 
 class SolarSystem extends React.Component {
   render() {
+    function retornaPlanetCard(imagem, nome) {
+      return (<PlanetCard planetImage={ imagem } planetName={ nome } />);
+    }
+    const planeta = Planets.map((p) => retornaPlanetCard(p.image, p.name));
     return (
       <div data-testid="solar-system">
         <Title headline="Planetas" />
+        <div id = "planetas">
+          { planeta }
+        </div>
       </div>
     );
   }
