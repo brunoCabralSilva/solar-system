c398f26204794d43df335776e218a8138212658c
brunoCabralSilva <bruno.cabral.silva2018@gmail.com>
2022-05-09 17:45:15 -0300

    Corrigindo css

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import '../App.css';
+import Title from './Title';
+import missions from '../data/missions';
+import MissionCard from './MissionCard';
 
 class Missions extends React.Component {
   render() {
+    function retornaCard(objeto) {
+      const { name: n, year: y, country: c, destination: d } = objeto;
+      return (<MissionCard name={ n } year={ y } country={ c } destination={ d } />);
+    }
+    const missao = missions.map((mission) => retornaCard(mission));
     return (
-      <div data-testid="missions"> </div>
+      <div data-testid="missions">
+        <Title headline="Missões" />
+        {missao}
+      </div>
     );
   }
 }
